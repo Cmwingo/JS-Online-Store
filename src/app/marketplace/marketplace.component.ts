d7149fbe8da50b3ddd32ecf08340c8dc9afa5ff2
Chris Wingo <cmwingo@gmail.com>
2017-01-23 14:57:22 -0800

    Allow for persistant data in Firebase and allow users to enter new albums

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 
 @Component({
   selector: 'app-marketplace',
@@ -13,7 +15,7 @@ export class MarketplaceComponent implements OnInit {
 
   constructor(private router: Router, private albumService: AlbumService) {}
 
-  albums: Album[] = [ ];
+  albums: FirebaseListObservable<any[]>;
 
   ngOnInit() {
     this.albums = this.albumService.getAlbums();
