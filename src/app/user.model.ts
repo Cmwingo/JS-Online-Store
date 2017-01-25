2c7a53cfec28eecc59c77f8a333266a07010fb4b
Wingo <cmwingo@gmail.com>
2017-01-24 20:34:57 -0800

    Be a mess but have some good things cooking

---
+++ src/app/user.model.ts
@@ -0,0 +1,3 @@
+export class User {
+  constructor (public userName: string, public email: string, public anonymous: boolean, public uniqueId: string) { }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Output } from '@angular/core';
 import { AngularFire, AngularFireModule, FIREBASE_PROVIDERS, FirebaseRef } from 'angularfire2';
 import { masterFirebaseConfig } from '../api-keys';
 import { firebaseAuthConfig } from '../app.module';
+import { User } from '../user.model';
 
 
 @Component({
@@ -10,18 +11,29 @@ import { firebaseAuthConfig } from '../app.module';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  currentUser: User;
  constructor(public af: AngularFire) {
-   this.af.auth.subscribe(auth => console.log(auth));
+   this.af.auth.subscribe(auth => this.currentUser = {
+     userName: auth.google.displayName,
+     email: auth.google.email,
+     anonymous: false,
+     uniqueId: auth.google.uid
+   });
+   let obj: any = FirebaseRef;                     //This is good code here
+   let auth: any = obj.$firebaseAuth();             //Returns no errors so object
+   auth.$signInWithRedirect(firebaseAuthConfig);  //does contain the methods
  }
 
+ @Output() userEmitter;
 
   login() {
     console.log("Logging in");
     this.af.auth.login();
-    console.log("Logged in");
+    console.log("Current User" + this.currentUser.userName);
   }
 
   logout() {
     this.af.auth.logout();
+    console.log("Current User" + this.currentUser.userName);
   }
 }
